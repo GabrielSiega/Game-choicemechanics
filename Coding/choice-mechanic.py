7006fd00e124e65c864e0bc92861a6fae7485585
Gabriel Siega <82450712+GabrielSiega@users.noreply.github.com>
2023-07-03 23:34:49 +0800

    Update choice-mechanic.py

---
+++ Coding/choice-mechanic.py
@@ -0,0 +1,41 @@
+
+
+
+
+#|Step x :Character choosing  |#
+print("")
+print("") #spacing
+print("_________________________________________________")
+print("       Player, Please Choose your Character :    ")
+print(" |Character1| |Character2| |Character3| |Character| ") #Character choice
+print("      1             2           3            4   ") #Number choice
+print("_________________________________________________")
+print()
+print("input:")
+
+
+user_input_4 = input()
+
+#Characters_in_game = ['....','....','....','....']
+#Choice In Character#
+Character_1_choice = "someman"
+Character_2_choice = "someman2"
+
+
+#Choice as a number
+Character_1_no = "1" #Some character
+Character_2_no = "2" #some character
+
+
+Character_1_choice_1 = Character_1_no
+Character_2_choice_2 = Character_2_no
+
+
+
+if user_input_4 == Character_1_choice_1 or Character_1_no:
+    print("You chose","Character1","Arm up !")
+    print("----------------------------------------")
+    
+elif user_input_4 == Character_2_choice_2 or Character_2_no:
+    print("You chose","Character 2","Arm up !")
+    print("----------------------------------------")
